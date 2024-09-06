ff2a50dcc6bbb70899ac57fcb77d0cd6d7a4892b
li xiaochao <gzlixiaochao@163.com>
2024-09-06 15:02:05 +0800

    头条

--- source/toutiao.py
+++ source/toutiao.py
@@ -13,7 +13,7 @@ from llm.baidu_qianfan import simple_chat, news_summary, simple_chat_app
 from uitls import post_mongodb
 from uitls import tools
 
-from seleitum_toutiao import seleitum_page
+from .seleitum_toutiao import seleitum_page
 
 # 全局浏览器实例
 browser = None
